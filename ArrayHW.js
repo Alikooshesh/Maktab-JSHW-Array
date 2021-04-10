aa0867c95f3d0f141eab6ef4cbdb20499730aff8
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-04-10 18:07:00 +0430

    add array combiner

--- ArrayHW.js
+++ ArrayHW.js
@@ -1,4 +1,18 @@
 //1 : Function for delete an index from array
 function deleteByIndex(arr,i) {
     arr.splice(i,1)
+    return arr
+}
+
+//2 : Function for combine arrays
+function combiner(...arrays) {
+    tempArr=[]
+
+    for (const arr of arrays) {
+        for (const key of arr) {
+            tempArr.push(key)
+        }
+    }
+
+    return(tempArr);
 }
